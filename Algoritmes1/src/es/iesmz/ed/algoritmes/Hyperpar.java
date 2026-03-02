37947565dbd10f21382432edcab76e1c0fbd4a1d
javivuvix2007-cmd <javivuvix2007@gmail.com>
2026-03-02 08:55:32 +0100

    Update Hyperpar.java

--- Algoritmes1/src/es/iesmz/ed/algoritmes/Hyperpar.java
+++ Algoritmes1/src/es/iesmz/ed/algoritmes/Hyperpar.java
@@ -1,13 +1,13 @@
+package es.iesmz.ed.algoritmes;
+
 public class Hyperpar {
     long variable;
 
     public Hyperpar(long variable) {
         this.variable = variable;
     }
-}
+
     public boolean esHyperPar(long variable) {
         return variable % 2 == 0;
     }
-
-void main() {
 }
